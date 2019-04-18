fec338709ea55c36753316fd2e336a64bfc0d3d1
Saurabh Sangpal <saurabhsangpal@gmail.com>
2019-04-18 17:17:52 +0530

    Initial Commit Implemented player movement bound to WASD and numpad 8426. TODO: Implement collision detection 🔥

---
+++ Assets/Scripts/PlayerControllerBoy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Bound wasd for movement. Arrow keys are not bound
+public class PlayerControllerBoy : MonoBehaviour
+{
+    float speed = 4.0f;
+    Vector3 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerControllerGirl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Bound the numpad keys for movement.
+public class PlayerControllerGirl : MonoBehaviour
+{
+    float speed = 8.0f;
+    Vector3 move;
+    float vertical, horizontal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("[8]"))
+        {
+            // Move up
+            vertical = 1;
+        } else if (Input.GetKey("[2]"))
+        {
+            // Move down
+            vertical = -1;
+        } else { vertical = 0; }
+        if (Input.GetKey("[4]"))
+        {
+            // Move left
+            horizontal = -1;
+        } else if (Input.GetKey("[6]"))
+        {
+            // Move right
+            horizontal = 1;
+        } else { horizontal = 0; }
+        move = new Vector2(horizontal, vertical);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
