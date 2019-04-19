e6aa56591bf71eec87e8bdd2cdc4ba76721ebca7
Saurabh Sangpal <saurabhsangpal@gmail.com>
2019-04-19 20:00:36 +0530

    Added customers and replaced two player scripts with just one. Adding gameplay to customers is still remaining.

---
+++ Assets/Scripts/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+    List<int> numbers = new List<int>();
+    public string[] Veg = new string[] { "Tomato", "Potato", "Cucumber", "Carrot", "Cabbage", "Cauliflower"};
+    // Automatically generates an order
+    void GenerateOrder()
+    {
+        int a = 0;
+        for (int i = 0; i < 3; i++) { 
+            while (a == 0)
+            {
+        	    a = Random.Range(0, 6);
+        	    if (!numbers.Contains(a))
+        	    {
+        	        numbers.Add(a);
+        	    } else
+        	    {
+        	        a = 0;
+        	    }
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateOrder();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string up, down, left, right, special;
+    public float speed;
+    Vector3 move;
+    float vertical, horizontal;
+
+    // Sets transformation depending on which key we have held down
+    void SetMovement()
+    {
+        if (Input.GetKey(up))
+        {
+            vertical = 1;
+        } else if (Input.GetKey(down))
+        {
+            vertical = -1;
+        } else
+        {
+            vertical = 0;
+        }
+        if (Input.GetKey(right))
+        {
+            horizontal = 1;
+        } else if (Input.GetKey(left))
+        {
+            horizontal = -1;
+        } else
+        {
+            horizontal = 0;
+        }
+        move = new Vector2(horizontal, vertical);
+        transform.position += move * speed * Time.deltaTime;
+    }
+
+    // SPACE for player 1 and RETURN for player 2
+    bool SpecialKey()
+    {
+        if (Input.GetKey(special))
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpecialKey();
+        SetMovement();
+    }
+}
