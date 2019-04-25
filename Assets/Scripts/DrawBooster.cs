48a8989b11c55b26123b71759825ce5c7516ae81
Saurabh Sangpal <saurabhsangpal@gmail.com>
2019-04-25 13:21:53 +0530

    Added images for booster and set up groundwork for displaying them Created DrawBooster.cs and set up a bool ShowBooster that handles if the game will display the image corresponding to the booster. Need to set up randomization of its location and bind the booster pickup to the player.👌👏

---
+++ Assets/Scripts/DrawBooster.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawBooster : MonoBehaviour
+{
+    public Player player1, player2; // Assign in inspector
+    private Image image;
+
+    // Will handle the position of the displayed image
+    private Vector3 move;
+
+    public bool ShowBooster;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        ShowBooster = false;
+    }
+
+    /// <summary>
+    /// Checks the bool ShowBooster and displays/doesn't display the image for
+    /// the booster accordingly
+    /// </summary>
+    void DisplayImage()
+    {
+        if (ShowBooster)
+        {
+            image.gameObject.SetActive(true);
+        }
+        else
+        {
+            image.gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        DisplayImage();
+    }
+}
