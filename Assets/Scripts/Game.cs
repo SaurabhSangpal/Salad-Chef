7fb05bc4115c373ec85eec252a105424945090f6
Saurabh Sangpal <saurabhsangpal@gmail.com>
2019-04-28 12:16:25 +0530

    Created a main menu

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,27 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
     private bool Paused = false;
-    [SerializeField]
-    Player player1, player2;
-    [SerializeField]
-    Text gametimer;
+    public Player player1, player2;
+    private float Timer;
+
+    void Start()
+    {
+        Timer = 300.0f;
+    }
 
     /// <summary>
     /// Explicitly tells the game to pause
     /// </summary>
-    public void PauseGame()
+    private void PauseGame()
     {
         Paused = true;
+    }
+
+    private void EndGame()
+    {
+        if (player1.TimeLeft <= 0f || player2.TimeLeft <= 0f)
+        {
+            PauseGame();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Pause if player presses Esc
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Paused = !Paused;
@@ -31,5 +42,13 @@ public class Game : MonoBehaviour
             Time.timeScale = 0;
         } else
             Time.timeScale = 1;
+
+        // Update timer
+        if (Timer >= 0)
+        {
+            Timer -= Time.deltaTime;
+        } else
+            PauseGame();
+        EndGame();
     }
 }
